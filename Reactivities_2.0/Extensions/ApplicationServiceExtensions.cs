bda0515d145b666fdcd51627acdc727efb805650
Paulo Braz <ph_braz@hotmail.com>
2021-10-18 19:10:55 +0100

    Added Mediator to our Application

---
+++ Reactivities_2.0/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Application.Activities;
+
+namespace Reactivities_2._0.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            //This allow we make calls from another port diff from our BE app
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            //adding mediator where to find our handler
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+
+        }
+    }
+}
--- Reactivities_2.0/Startup.cs
+++ Reactivities_2.0/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Reactivities_2._0.Extensions;
 
 namespace Reactivities_2._0
 {
@@ -26,19 +21,9 @@ namespace Reactivities_2._0
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config);
+            
 
-            //This allow we make calls from another port diff from our BE app
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
