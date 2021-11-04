66c684aa725d8a3621ae4b723c5056c34f04bed8
Paulo Braz <ph_braz@hotmail.com>
2021-11-04 19:32:59 +0000

    Closes navbarfix-01:  Added new functionalities to handle delete view and create new acitivities locally and styling to our navbar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from '../layout/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
+
 
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>("http://localhost:34876/api/activities").then(response => {
@@ -14,18 +19,54 @@ function App() {
     })
   }, [])
 
-  return (
-    <div>
-      <NavBar /> 
-        <List>
-        {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
 
-        </List>
-    </div>
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?:string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
+    : setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+
+  }
+
+
+  function handleDeleteActivitiy(id: string){
+    setActivities([...activities.filter(x => x.id !== id)]);
+  }
+
+  return (
+    <>
+      <NavBar openForm={handleFormOpen} /> 
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivitiy}
+          
+        />
+      </Container>
+    </>
   );
 }
 
