e1f327b85cc44384540d493d65493e6ab441dfcb
Paulo Braz <ph_braz@hotmail.com>
2021-11-06 19:45:17 +0000

    Added mobx state management to our app

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,56 +1,31 @@
 import React from "react";
 import { Grid} from "semantic-ui-react";
-import {Activity} from '../../../app/models/activity';
 import AcitivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../forms/ActivityForm';
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
 
-interface Props {
-    activities : Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id : string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean
-}
 
-export default function ActivityDashboard({activities, selectedActivity, submitting,
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props ) {
+
+export default observer(function ActivityDashboard() {
+
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode } = activityStore;
+
     return (
         <Grid>
             <Grid.Column width='10'>
-                <AcitivityList 
-                    activities={activities} 
-                    selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-
-                
-                />
+                <AcitivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
-                <ActivityDetails 
-                    activity={selectedActivity} 
-                    cancelSelectActivity={cancelSelectActivity}
-                    openForm={openForm}
-                    
-                /> }
+                <ActivityDetails /> }
                 {editMode && 
-                <ActivityForm 
-                    closeForm={closeForm} 
-                    activity={selectedActivity} 
-                    createOrEdit={createOrEdit}
-                    submitting={submitting}
-                
-                /> }
+                <ActivityForm /> }
             </Grid.Column>
 
         </Grid>
     )
-}
+})
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,17 +1,17 @@
+import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import {Activity} from '../../../app/models/activity';
+import { useStore } from "../../../app/stores/store";
 
-interface Props{
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean
-}
 
-export default function AcitivityList({activities, selectActivity, deleteActivity, submitting}: Props){
+
+export default observer(function AcitivityList(){
+    const {activityStore} = useStore();
+    const {deleteActivity,activitiesByDate , loading } = activityStore;
+
 
     const [target, setTarget] = useState('');
+
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
         setTarget(e.currentTarget.name);
@@ -23,7 +23,7 @@ export default function AcitivityList({activities, selectActivity, deleteActivit
     return(
         <Segment>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -34,9 +34,9 @@ export default function AcitivityList({activities, selectActivity, deleteActivit
                             </Item.Description>
                         </Item.Content>
                         <Item.Extra>
-                            <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                            <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
                             <Button 
-                                loading={submitting && target === activity.id} 
+                                loading={loading && target === activity.id} 
                                 onClick={(e) => handleActivityDelete(e, activity.id)} 
                                 floated='right' 
                                 content='Delete' 
@@ -51,4 +51,4 @@ export default function AcitivityList({activities, selectActivity, deleteActivit
         </Segment>
     )
 
-}
+})
