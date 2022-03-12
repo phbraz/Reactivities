b9dddd6647b043e3cda30f90136c181ddf21688e
Paulo Braz <ph_braz@hotmail.com>
2022-03-12 22:53:26 +0000

    Added formik and datepicker to our app

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import {format} from "date-fns";
 
 
 export default class ActivityStore {
@@ -8,7 +9,7 @@ export default class ActivityStore {
     selectedActivity: Activity  | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
 
     constructor() {
@@ -18,13 +19,13 @@ export default class ActivityStore {
 
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => 
-            Date.parse(a.date) - Date.parse(b.date))
+            a.date!.getTime() - b.date!.getTime())
     }
 
     get groupActivities(){
         return Object.entries(
             this.activitiesByDate.reduce((activities, activity) => {
-                const date = activity.date;
+                const date = format(activity.date!, 'dd MMM yyyy h:mm aa');
                 activities[date] = activities[date] ? [...activities[date], activity] : [activity];
                 
                 return activities;
@@ -77,7 +78,7 @@ export default class ActivityStore {
     }
 
     private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split('T')[0];
+        activity.date = new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
 
     }
